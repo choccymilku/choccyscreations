b5de2b397fec318da6ec2a928d3cd16411595a65
choccymilk <99021003+choccymilku@users.noreply.github.com>
2023-06-11 22:17:53 +0200

    ywea

--- code/apis/discord.js
+++ code/apis/discord.js
@@ -75,15 +75,16 @@ const activities = data.activities;
       statusCaseDiv.style.backgroundColor = '#23a459';
   }
 
-/* const listeningToSpotify = activities ? activities.some(activity => activity.type === 2 && activity.name === 'Spotify') : false;
+const listeningToSpotify = activities ? activities.some(activity => activity.type === 2 && activity.name === 'Spotify') : false;
 
 if (listeningToSpotify) {
   const spotifyActivity = activities.find(activity => activity.type === 2 && activity.name === 'Spotify');
 
   setTimeout(() => {
-    document.getElementById('listening-to-spotify').style.opacity = '1';
-  document.getElementById('discord-activity-section').style.opacity = '1';
-  }, 300);
+    document.getElementById('discord-activity-section').style.opacity = '1';
+    document.getElementById('discord-activity-section').style.height = '100px';
+    document.getElementById('username').style.marginTop = '0px';
+  }, 1250);
   const spotifyActivityDiv = document.getElementById('listening-to-spotify');
   const trackId = spotifyActivity.sync_id;
   const songName = spotifyActivity.details;
@@ -91,25 +92,28 @@ if (listeningToSpotify) {
   const album = spotifyActivity.assets.large_text;
   const albumCover = `https://i.scdn.co/image/${spotifyActivity.assets.large_image.slice(8)}`;
 
-  const spotifyActivityTitle = document.getElementById('listening-to-spotify-text');
-  spotifyActivityTitle.innerHTML = 
-  'LISTENING TO SPOTIFY' + 
-  '<div style="width: 34px;height: 32px;background-color:var(--tooltip-background);z-index:1;position: relative;border-bottom-left-radius: 50%;border-top-right-radius:8px;margin-top:-10px;float:right;"><i style="margin-right:8px;margin-top:6px;font-size:20px;float:right;color:#1ed760;" class="fa-brands fa-spotify"></i></div>';
   const songNameElement = document.getElementById('listening-to-spotify-song');
   songNameElement.innerText = songName;
+  const songNameElement2 = document.getElementById('listening-to-spotify-song2');
+  songNameElement2.innerText = songName;
 
   const artistElement = document.getElementById('listening-to-spotify-artist');
   artistElement.innerText = 'by ' + artist;
-
-  const albumElement = document.getElementById('listening-to-spotify-album');
-  albumElement.innerText = 'on ' + album;
+  const artistElement2 = document.getElementById('listening-to-spotify-artist2');
+  artistElement2.innerText = 'by ' + artist;
 
   const albumCoverElement = document.getElementById('listening-to-spotify-cover');
   albumCoverElement.src = albumCover;
   albumCoverElement.setAttribute('title', songName + '\n' + artist);
 
+  const albumCoverElement2 = document.getElementById('listening-to-spotify-cover2');
+  albumCoverElement2.src = albumCover;
+  albumCoverElement2.setAttribute('title', songName + '\n' + artist);
+
   const spotifyLinkElement = document.getElementById('play-on-spotify-link');
   spotifyLinkElement.href = `https://open.spotify.com/track/${trackId}`;
+  const spotifyLinkElement2 = document.getElementById('play-on-spotify-link2');
+  spotifyLinkElement2.href = `https://open.spotify.com/track/${trackId}`;
 
   // Get the initial timestamps and calculate the total time
   const spotifyTimestamps = spotifyActivity.timestamps;
@@ -119,15 +123,19 @@ if (listeningToSpotify) {
 
   // Check if an elapsed time element already exists and remove it
   const spotifyElapsedTimeWrapper = document.getElementById('listening-to-spotify-elapsed-time-wrapper');
+  const spotifyElapsedTimeWrapper2 = document.getElementById('listening-to-spotify-elapsed-time-wrapper2');
   if (spotifyElapsedTimeWrapper) {
     spotifyElapsedTimeWrapper.remove();
+  }
+  if (spotifyElapsedTimeWrapper2) {
+    spotifyElapsedTimeWrapper2.remove();
   }
 
   // Create a new element for the elapsed time
   const spotifyElapsedTimeWrapperNew = document.createElement('div');
   spotifyElapsedTimeWrapperNew.id = 'listening-to-spotify-elapsed-time-wrapper';
-  spotifyElapsedTimeWrapperNew.style.marginLeft = '0px';
-  spotifyElapsedTimeWrapperNew.style.marginTop = '8px';
+  const spotifyElapsedTimeWrapperNew2 = document.createElement('div');
+  spotifyElapsedTimeWrapperNew2.id = 'listening-to-spotify-elapsed-time-wrapper2';
 
   const spotifyElapsedTimeDisplayLeft = document.createElement('h6');
   spotifyElapsedTimeDisplayLeft.id = 'listening-to-spotify-elapsed-time-left';
@@ -135,7 +143,12 @@ if (listeningToSpotify) {
   spotifyElapsedTimeDisplayLeft.style.overflow = 'hidden';
   spotifyElapsedTimeDisplayLeft.style.textOverflow = 'ellipsis';
   spotifyElapsedTimeDisplayLeft.style.fontSize = '1rem';
-
+  const spotifyElapsedTimeDisplayLeft2 = document.createElement('h6');
+  spotifyElapsedTimeDisplayLeft2.id = 'listening-to-spotify-elapsed-time-left2';
+  spotifyElapsedTimeDisplayLeft2.style.whiteSpace = 'nowrap';
+  spotifyElapsedTimeDisplayLeft2.style.overflow = 'hidden';
+  spotifyElapsedTimeDisplayLeft2.style.textOverflow = 'ellipsis';
+  spotifyElapsedTimeDisplayLeft2.style.fontSize = '1rem';
 
   const spotifyElapsedTimeDisplayRight = document.createElement('h6');
   spotifyElapsedTimeDisplayRight.id = 'listening-to-spotify-elapsed-time-right';
@@ -144,11 +157,20 @@ if (listeningToSpotify) {
   spotifyElapsedTimeDisplayRight.style.textOverflow = 'ellipsis';
   spotifyElapsedTimeDisplayRight.style.fontSize = '1rem';
   spotifyElapsedTimeDisplayRight.style.marginLeft = 'auto';
-
+  const spotifyElapsedTimeDisplayRight2 = document.createElement('h6');
+  spotifyElapsedTimeDisplayRight2.id = 'listening-to-spotify-elapsed-time-right2';
+  spotifyElapsedTimeDisplayRight2.style.whiteSpace = 'nowrap';
+  spotifyElapsedTimeDisplayRight2.style.overflow = 'hidden';
+  spotifyElapsedTimeDisplayRight2.style.textOverflow = 'ellipsis';
+  spotifyElapsedTimeDisplayRight2.style.fontSize = '1rem';
+  spotifyElapsedTimeDisplayRight2.style.marginLeft = 'auto';
 
   spotifyElapsedTimeWrapperNew.appendChild(spotifyElapsedTimeDisplayLeft);
   spotifyElapsedTimeWrapperNew.appendChild(spotifyElapsedTimeDisplayRight);
-  artistElement.parentNode.insertBefore(spotifyElapsedTimeWrapperNew, albumElement.nextSibling);
+  artistElement.parentNode.insertBefore(spotifyElapsedTimeWrapperNew, artistElement.nextSibling);
+  spotifyElapsedTimeWrapperNew2.appendChild(spotifyElapsedTimeDisplayLeft2);
+  spotifyElapsedTimeWrapperNew2.appendChild(spotifyElapsedTimeDisplayRight2);
+  artistElement2.parentNode.insertBefore(spotifyElapsedTimeWrapperNew2, artistElement2.nextSibling);
 
   const updateElapsedTime = () => {
     // Get the latest timestamps and calculate the elapsed time
@@ -173,13 +195,19 @@ if (listeningToSpotify) {
   
     spotifyElapsedTimeDisplayLeft.innerText = leftTimeDisplay;
     spotifyElapsedTimeDisplayRight.innerText = rightTimeDisplay;
+    spotifyElapsedTimeDisplayLeft2.innerText = leftTimeDisplay;
+    spotifyElapsedTimeDisplayRight2.innerText = rightTimeDisplay;
   };
 
 // Check if a progress bar element already exists and remove it
 const spotifyProgressBarWrapper = document.getElementById('listening-to-spotify-progress-bar-wrapper');
+const spotifyProgressBarWrapper2 = document.getElementById('listening-to-spotify-progress-bar-wrapper2');
 
 if (spotifyProgressBarWrapper) {
   spotifyProgressBarWrapper.remove();
+}
+if (spotifyProgressBarWrapper2) {
+  spotifyProgressBarWrapper2.remove();
 }
 
 // Create a new element for the progress bar
@@ -187,18 +215,30 @@ const spotifyProgressBarWrapperNew = document.createElement('div');
 spotifyProgressBarWrapperNew.id = 'listening-to-spotify-progress-bar-wrapper';
 spotifyProgressBarWrapperNew.style.width = '100%';
 spotifyProgressBarWrapperNew.style.height = '4px';
-spotifyProgressBarWrapperNew.style.backgroundColor = 'var(--spotify-progress-background)';
+spotifyProgressBarWrapperNew.style.backgroundColor = 'var(--background)';
 spotifyProgressBarWrapperNew.style.borderRadius = '4px';
-spotifyProgressBarWrapperNew.style.marginTop = '4px';
+const spotifyProgressBarWrapperNew2 = document.createElement('div');
+spotifyProgressBarWrapperNew2.id = 'listening-to-spotify-progress-bar-wrapper2';
+spotifyProgressBarWrapperNew2.style.width = '100%';
+spotifyProgressBarWrapperNew2.style.height = '4px';
+spotifyProgressBarWrapperNew2.style.backgroundColor = 'var(--background)';
+spotifyProgressBarWrapperNew2.style.borderRadius = '4px';
 
 const spotifyProgressBar = document.createElement('div');
 spotifyProgressBar.style.width = '0%';
 spotifyProgressBar.style.height = '100%';
-spotifyProgressBar.style.backgroundColor = 'var(--spotify-progress)';
+spotifyProgressBar.style.backgroundColor = 'var(--iconcolor)';
 spotifyProgressBar.style.borderRadius = '4px';
+const spotifyProgressBar2 = document.createElement('div');
+spotifyProgressBar2.style.width = '0%';
+spotifyProgressBar2.style.height = '100%';
+spotifyProgressBar2.style.backgroundColor = 'var(--iconcolor)';
+spotifyProgressBar2.style.borderRadius = '4px';
 
 spotifyProgressBarWrapperNew.appendChild(spotifyProgressBar);
-artistElement.parentNode.insertBefore(spotifyProgressBarWrapperNew, albumElement.nextSibling);
+artistElement.parentNode.insertBefore(spotifyProgressBarWrapperNew, artistElement.nextSibling);
+spotifyProgressBarWrapperNew2.appendChild(spotifyProgressBar2);
+artistElement2.parentNode.insertBefore(spotifyProgressBarWrapperNew2, artistElement2.nextSibling);
 
 const updateProgressBar = () => {
   // Get the latest timestamps and calculate the elapsed time
@@ -210,6 +250,7 @@ const updateProgressBar = () => {
 
   // Set the width of the progress bar to the calculated percentage
   spotifyProgressBar.style.width = `${elapsedPercentage}%`;
+  spotifyProgressBar2.style.width = `${elapsedPercentage}%`;
 };
 
 updateProgressBar();
@@ -218,19 +259,13 @@ setInterval(updateProgressBar, 1000);
 updateElapsedTime();
 setInterval(updateElapsedTime, 1000);
  
-document.getElementById('discord-about-connections-container').style.marginTop = '-4px';
-document.getElementById('discord-activity-section').style.height = '190px';
-document.getElementById('bottom-spotify-divider').style.height = '1px';
 } else {
   setTimeout(() => {
-    document.getElementById('listening-to-spotify').style.opacity = '0';
   document.getElementById('discord-activity-section').style.opacity = '0';
-  }, 300);
   document.getElementById('discord-activity-section').style.height = '0px';
-  document.getElementById('bottom-spotify-divider').style.height = '0px';
-  document.getElementById('discord-activity-section').style.opacity = '0';
-  document.getElementById('discord-about-connections-container').style.marginTop = '-16px';
-} */
+  document.getElementById('username').style.marginTop = '60px';
+  }, 1250);
+}
 
   const discordUser = data.discord_user;
   if (discordUser) {
@@ -255,55 +290,4 @@ usernameTagElement.innerText = '#' + discordtag;
 usernameTagElement.style.color = 'var(--text)';
 usernameElement.appendChild(usernameTagElement); // Append the <span> element to the <h6> element
 
-
-// Get the <h6> element
-const h6Element = document.getElementById("username");
-
-// Get the copy button element
-const copyButton = document.getElementById("username");
-
-// Define the copyTextToClipboard function
-function copyTextToClipboard() {
-  // Get the dynamic text from the <h6> element
-  const dynamicText = h6Element.innerText;
-
-  // Create a temporary <textarea> element
-  const textarea = document.createElement("textarea");
-  textarea.value = dynamicText;
-
-  // Append the <textarea> element to the document
-  document.body.appendChild(textarea);
-
-  // Select the text in the <textarea> element
-  textarea.select();
-
-  // Copy the selected text to the clipboard
-  document.execCommand("copy");
-
-  // Remove the <textarea> element from the document
-  document.body.removeChild(textarea);
-
-  // Change the text within the button to "Copied to clipboard"
-  copyButton.innerText = "copied!";
-
-  // Remove the event listener from the copy button for 1 second
-  copyButton.removeEventListener("click", copyTextToClipboard);
-  setTimeout(function() {
-    copyButton.addEventListener("click", copyTextToClipboard);
-    copyButton.innerText = discordusername;
-    const usernameTagElement = document.createElement('span'); // Create the <span> element
-    usernameTagElement.id = 'discord-username-tag'; // Set its ID to 'discord-username-tag'
-    usernameTagElement.innerText = '#' + discordtag;
-    usernameTagElement.style.color = '#b5bac1';
-    copyButton.appendChild(usernameTagElement); // Append the <span> element to the <h6> element
-  }, 1000);
-
-  // Add a class to the copy button for 2 seconds, then remove it
-  copyButton.classList.add("discord-user-copied-anim");
-  setTimeout(function() {
-    copyButton.classList.remove("discord-user-copied-anim");
-  }, 1000);
-}
-
-// Add an event listener to the copy button
-copyButton.addEventListener("click", copyTextToClipboard);}}
+}}
