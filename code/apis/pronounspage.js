4b3a0e2fab8d3d6190d723d8452ffc5d2ad10224
choccymilk <99021003+choccymilku@users.noreply.github.com>
2023-06-18 00:14:05 +0200

    ye

--- code/apis/pronounspage.js
+++ code/apis/pronounspage.js
@@ -104,12 +104,6 @@ pronounspagePronounsContainer.appendChild(pronounSpan);
         }
       });
     }
-
-    const timezone = data.profiles.en.timezone.tz;
-    const timezoneContainer = document.getElementById('timezone');
-    timezoneContainer.textContent = timezone;
-
-  
   })
   .catch(error => {
     // Handle any errors
