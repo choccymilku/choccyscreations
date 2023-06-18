77dce1328416bea0a3d87ff9c12502244aa68d9c
choccymilk <99021003+choccymilku@users.noreply.github.com>
2023-06-18 21:50:39 +0200

    yea

--- code/apis/pronounspage.js
+++ code/apis/pronounspage.js
@@ -1,4 +1,3 @@
-const username = 'chocolateychoccy';
 const url = `https://en.pronouns.page/api/profile/get/${username}?version=2`;
 
 fetch(url)
@@ -28,41 +27,52 @@ pronounspagePronounsContainer.appendChild(pronounSpan);
 
 
 
-    const generateFlagElement = (flag) => {
-      const flagUrl = `https://en.pronouns.page/flags/${flag}.png`;
-      const terminologyUrl = `https://en.pronouns.page/terminology#${flag}`;
-      const flagLink = document.createElement('a');
-      flagLink.href = terminologyUrl;
-      flagLink.target = '_blank';
-      flagLink.textContent = flag.toLowerCase() + ' '; // Add an extra space after the flag name
-      flagLink.className = 'flag-name';
+const generateFlagElement = (flag) => {
+  const flagUrl = `https://en.pronouns.page/flags/${flag}.png`;
+  const terminologyUrl = `https://en.pronouns.page/terminology#${flag}`;
+  const flagLink = document.createElement('a');
+  flagLink.href = terminologyUrl;
+  flagLink.target = '_blank';
+  
+  // Remove non-alphabetical characters from the flag name
+  const cleanFlagName = flag.toLowerCase().replace(/[^a-z]+/g, '');
+  flagLink.textContent = cleanFlagName + ' '; // Add an extra space after the flag name
+  flagLink.className = 'flag-name';
 
-      const flagImg = document.createElement('img');
-      flagImg.src = flagUrl;
-      flagImg.alt = flag;
-      flagImg.className = 'flag-emojis';
+  const flagImg = document.createElement('img');
+  flagImg.src = flagUrl;
+  flagImg.alt = flag;
+  flagImg.className = 'flag-emojis';
 
-      flagLink.appendChild(flagImg);
-      return flagLink;
-    };
+  flagLink.appendChild(flagImg);
+  return flagLink;
+};
 
-    const generateCustomFlagElement = (customFlag) => {
-      const customFlagUrl = `https://dclu0bpcdglik.cloudfront.net/images/${customFlag.value}-flag.png`;
-      const customFlagName = customFlag.name;
-      const customFlagLink = document.createElement('a');
-      customFlagLink.href = customFlag.link;
-      customFlagLink.target = '_blank';
-      customFlagLink.textContent = customFlagName.toLowerCase() + ' '; // Add an extra space after the flag name
-      customFlagLink.className = 'flag-name';
 
-      const customFlagImg = document.createElement('img');
-      customFlagImg.src = customFlagUrl;
-      customFlagImg.alt = customFlag.name;
-      customFlagImg.className = 'flag-emojis';
+const generateCustomFlagElement = (customFlag) => {
+  const customFlagUrl = `https://dclu0bpcdglik.cloudfront.net/images/${customFlag.value}-flag.png`;
+  const customFlagName = customFlag.name;
+  const customFlagLink = document.createElement('a');
+  customFlagLink.textContent = customFlagName.toLowerCase() + ' '; // Add an extra space after the flag name
 
-      customFlagLink.appendChild(customFlagImg);
-      return customFlagLink;
-    };
+  if (customFlag.link && customFlag.link.trim() !== '') {
+    customFlagLink.href = customFlag.link;
+    customFlagLink.target = '_blank';
+    customFlagLink.className = 'flag-name';
+  } else {
+    customFlagLink.className = 'flag-name-null';
+  }
+
+  const customFlagImg = document.createElement('img');
+  customFlagImg.src = customFlagUrl;
+  customFlagImg.alt = customFlag.name;
+  customFlagImg.className = 'flag-emojis';
+
+  customFlagLink.appendChild(customFlagImg);
+  return customFlagLink;
+};
+
+
 
     // Combine flags and customFlags into one array
     const allFlags = Array.isArray(flags) ? flags : [flags];
@@ -104,7 +114,65 @@ pronounspagePronounsContainer.appendChild(pronounSpan);
         }
       });
     }
+
+    const timezone = data.profiles.en.timezone.tz;
+        
+    
+    
+    function updateClock(timezone) {
+      const now = new Date();
+    
+      // Display time in the specified timezone
+      const timeOptions = {
+        hour12: localStorage.getItem('clockFormat') === '12-hour',
+        hour: '2-digit',
+        minute: '2-digit',
+        timeZone: timezone
+      };
+      const timeString = now.toLocaleTimeString([], timeOptions);
+      const formattedTime = timeString.replace(/(\d+:\d+)\s([ap]m)/i, '$1 $2').toUpperCase();
+    
+      // Display the day of the week
+      const dayOptions = {
+        weekday: 'long'
+      };
+      const dayString = now.toLocaleDateString([], dayOptions);
+    
+      const clockText = `${formattedTime} on a ${dayString}`;
+    
+      // Display user's offset in UTC
+      const userOffsetHours = Math.floor(now.getTimezoneOffset() / -60);
+      const userOffsetMinutes = Math.abs(now.getTimezoneOffset() % 60);
+      const offsetText = `UTC ${userOffsetHours >= 0 ? '+' : '-'}${Math.abs(userOffsetHours)}:${userOffsetMinutes.toString().padStart(2, '0')}`;
+    
+      // Update the clock element with the formatted time, day, and offset
+      clockElement.innerHTML = `${clockText} <span style="font-size:1rem">( ${offsetText} )</span>`;
+    }
+    
+    let clockElement = document.getElementById('clock');
+    if (!clockElement) {
+      const observer = new MutationObserver(mutations => {
+        mutations.forEach(mutation => {
+          if (mutation.addedNodes) {
+            for (let i = 0; i < mutation.addedNodes.length; i++) {
+              const node = mutation.addedNodes[i];
+              if (node.id === 'clock') {
+                clockElement = node;
+                setInterval(() => updateClock(timezone), 0);
+                observer.disconnect();
+                break;
+              }
+            }
+          }
+        });
+      });
+      observer.observe(document.documentElement, { childList: true, subtree: true });
+    } else {
+      setInterval(() => updateClock(timezone), 0);
+    }
   })
+
+  
   .catch(error => {
     // Handle any errors
     console.error('Error:', error);
--- code/config.js
+++ code/config.js
@@ -1,10 +1,10 @@
 const bday_day = '6';
 const bday_month = '9';
 const bday_year = '2007';
-let timezone = 'CET'; // Replace with your desired timezone
 const discord_user_id = '945717456163442708';
 const roblox_id = "2450458016";
 const mastodon_username = "";
+const username = "chocolateychoccy";
 
 var custom_text = 
 "call me choccy or choco, up to you\n" +
