0f47d9e73920eb91d5d90fadccd3173b1288d3a0
chocsteryu <99021003+choccymilku@users.noreply.github.com>
2023-08-06 23:11:08 +0200

    Update friends.js

--- code/apis/friends.js
+++ code/apis/friends.js
@@ -1,8 +1,8 @@
 const discordUserIds = [
-  //example user IDs
+  // example user IDs
   '669956621618642985', // vulpy
   '535185683325648904', // meow
-  '906701264887382027',  // Danny
+  '906701264887382027', // Danny
   '768656516885774371', // prax
   '984835890608230430', // vin
   '503676030536646716', // ashy
@@ -17,57 +17,39 @@ const friendsContainers = document.querySelectorAll('#friends_inner, #friends_in
 
 // check if data exists in local storage and is not older than 30 minutes
 const data = JSON.parse(localStorage.getItem('discordFriends'));
-if (data && Date.now() - data.timestamp < 360 * 60 * 1000) {
+if (data && Date.now() - data.timestamp < 30 * 60 * 1000) {
   // generate friend elements using the saved data
   friendsContainers.forEach(friendsContainer => {
     discordUserIds.forEach(id => {
       const friendData = data[id];
-      const avatarBG = document.createElement('div');
-      avatarBG.setAttribute('id', 'friends_avatar_bg');
-      /*         avatarBG.style.backgroundColor = friendData.banner.color; */
-  
-      const avatar = document.createElement('img');
-      avatar.setAttribute('src', `https://cdn.discordapp.com/avatars/${id}/${friendData.avatar.id}`);
-      avatar.setAttribute('id', 'friends_avatar');
-  
-      const name = document.createElement('h6');
-      name.textContent = friendData.tag.substring(0, friendData.tag.indexOf('#'));
-      name.setAttribute('id', 'friends_name');
-      name.title = friendData.tag.substring(0, friendData.tag.indexOf('#'));
-  
+      const formattedData = {
+        id: id,
+        username: friendData.tag.substring(0, friendData.tag.indexOf('#')),
+        avatar: `https://cdn.discordapp.com/avatars/${id}/${friendData.avatar.id}`
+      };
+
       const container = document.createElement('div');
       container.setAttribute('id', 'friends_container');
-      container.setAttribute('class', 'friends_container');
-      container.appendChild(avatar);
-  
+
+      const avatarBG = document.createElement('div');
+      avatarBG.setAttribute('id', 'friends_avatar_bg');
+
+      const avatar = document.createElement('img');
+      avatar.setAttribute('src', formattedData.avatar);
+      avatar.setAttribute('id', 'friends_avatar');
+
+      const name = document.createElement('h6');
+      name.textContent = formattedData.username;
+      name.setAttribute('id', 'friends_name');
+      name.title = formattedData.username;
+
       container.appendChild(avatarBG);
       avatarBG.appendChild(avatar);
       container.appendChild(name);
-  
-      friendsContainer.appendChild(container);
-  
-      if (id === '1035262868586766376') {
-        const link = document.createElement('a');
-        link.href = 'https://benreyland.crd.co';
-        link.textContent = friendData.tag.substring(0, friendData.tag.indexOf('#'));
-        link.target = '_blank';
-        name.textContent = '';
-        name.appendChild(link);
-      }
-      if (id === '981935028751695943') {
-        const link = document.createElement('a');
-        link.href = 'https://rentry.org/binarystarz';
-        link.textContent = friendData.tag.substring(0, friendData.tag.indexOf('#'));
-        link.target = '_blank';
-        name.textContent = '';
-        name.appendChild(link);
-      }
-  
-      container.appendChild(name);
+
       friendsContainer.appendChild(container);
     });
   });
-  
 } else {
   friendsContainers.forEach(friendsContainer => {
     Promise.all(discordUserIds.map(id => fetch(`https://discordlookup.noodlee.repl.co/api?id=${id}`)))
@@ -76,45 +58,33 @@ if (data && Date.now() - data.timestamp < 360 * 60 * 1000) {
         // generate friend elements
         data.forEach(friendData => {
           const id = friendData.id;
+          const formattedData = {
+            id: id,
+            username: friendData.tag.substring(0, friendData.tag.indexOf('#')),
+            avatar: `https://cdn.discordapp.com/avatars/${id}/${friendData.avatar.id}`
+          };
+
           const container = document.createElement('div');
           container.setAttribute('id', 'friends_container');
-  
+
           const avatarBG = document.createElement('div');
           avatarBG.setAttribute('id', 'friends_avatar_bg');
-          /*         avatarBG.style.backgroundColor = friendData.banner.color; */
-  
+
           const avatar = document.createElement('img');
-          avatar.setAttribute('src', `https://cdn.discordapp.com/avatars/${id}/${friendData.avatar.id}`);
+          avatar.setAttribute('src', formattedData.avatar);
           avatar.setAttribute('id', 'friends_avatar');
-  
+
           const name = document.createElement('h6');
-          name.textContent = friendData.tag.substring(0, friendData.tag.indexOf('#'));
+          name.textContent = formattedData.username;
           name.setAttribute('id', 'friends_name');
-          name.title = friendData.tag.substring(0, friendData.tag.indexOf('#'));
-  
+          name.title = formattedData.username;
+
           container.appendChild(avatarBG);
           avatarBG.appendChild(avatar);
           container.appendChild(name);
-  
-          if (id === '1035262868586766376') {
-            const link = document.createElement('a');
-            link.href = 'https://benreyland.crd.co';
-            link.textContent = friendData.tag.substring(0, friendData.tag.indexOf('#'));
-            link.target = '_blank';
-            name.textContent = '';
-            name.appendChild(link);
-          }
-          if (id === '981935028751695943') {
-            const link = document.createElement('a');
-            link.href = 'https://rentry.org/binarystarz';
-            link.textContent = friendData.tag.substring(0, friendData.tag.indexOf('#'));
-            link.target = '_blank';
-            name.textContent = '';
-            name.appendChild(link);
-          }
-  
+
           friendsContainer.appendChild(container);
-  
+
           const savedData = JSON.parse(localStorage.getItem('discordFriends')) || {};
           savedData[id] = friendData;
           savedData.timestamp = Date.now();
@@ -122,9 +92,9 @@ if (data && Date.now() - data.timestamp < 360 * 60 * 1000) {
         });
       })
       .catch(error => console.error(error));
-  
+
     // Add console log for the next fetch
     const timeUntilNextFetch = (Date.now() - data.timestamp) / 1000;
     console.log(`Next fetch in ${timeUntilNextFetch} seconds.`);
   });
-}  
+}
